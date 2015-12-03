a0bd5b75272b3aecb033d9b41c66c8b4c8855f4b
Thomas Wassum <thomas.e.wassum@gmail.com>
2015-12-02 19:56:48 -1000

    Enhanced login to require password reset if password equals 'password', the default password

--- login/views.py
+++ login/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User as uauth
 from django.core.urlresolvers import reverse
 from .forms import Login, ResetPassword
 from register.models import User as u, Person, Facility
-
+from register.views import passwordcheck
 
 # Create your views here.
 
@@ -17,23 +18,30 @@ def loginscreen(request):
                 username = u.objects.get(pk=form.cleaned_data['username'])
                 person = Person.objects.get(email=username.username)
                 user = authenticate(username=username, password = form.cleaned_data['password'])
-                if user.is_active:
-                    login(request, user)
-                    request.session['personpk'] = person.pk
-                    request.session['role'] = person.role_id
-                    request.session['fac'] = person.facility_id
-                    if person.role_id == 1:
-                        request.session['schoolfilter'] = 'all'
-                        request.session['island'] = 'all'
-                        request.session['district'] = 'all'
-                    if(person.facility_id > 0):
-                        f = Facility.objects.get(pk = person.facility_id)
-                        request.session['hasprek'] = f.has_pre_k
-                        request.session['onlyprk'] = f.is_only_pre_k
-                    request.session['lname'] = 'all'
-                    request.session['ulname'] = 'all'
+                if user:
+                    if user.is_active:
+                        login(request, user)
+                        request.session['personpk'] = person.pk
+                        request.session['role'] = person.role_id
+                        request.session['fac'] = person.facility_id
+                        if person.role_id == 1:
+                            request.session['schoolfilter'] = 'all'
+                            request.session['island'] = 'all'
+                            request.session['district'] = 'all'
+                        if(person.facility_id > 0):
+                            f = Facility.objects.get(pk = person.facility_id)
+                            request.session['hasprek'] = f.has_pre_k
+                            request.session['onlyprk'] = f.is_only_pre_k
+                        request.session['lname'] = 'all'
+                        request.session['ulname'] = 'all'
                     if person.verified:
-                        return render(request,'login/landingpage.html', {'p':person})
+                        username = u.objects.get(pk = person.email_id)
+                        if username.password == 'password':
+                            return HttpResponseRedirect(reverse('login:reset'))
+                        else:
+                            return render(request,'login/landingpage.html', {'p':person})
+                else:
+                    HttpResponseRedirect('')
             except u.DoesNotExist:
                 HttpResponseRedirect('')
 
@@ -51,11 +59,18 @@ def resetpassword(request):
     if request.method == 'POST':
         form = ResetPassword(request.POST)
         if form.is_valid():
+            pw1 = form.cleaned_data['new_password1']
+            pw2 = form.cleaned_data['new_password2']
+            oldpw = form.cleaned_data['old_password']
+            pwcheck = passwordcheck(pw1)
             user = u.objects.get(pk = form.cleaned_data['username'])
-            if user.password == form.cleaned_data['old_password']:
-                if form.cleaned_data['new_password1'] == form.cleaned_data['new_password1']:
+            if user.password == oldpw:
+                if pw1 == pw2 and pwcheck != 1:
                     user.password = form.cleaned_data['new_password2']
                     user.save()
+                    auth_user = uauth.objects.get(username = form.cleaned_data['username'])
+                    auth_user.set_password(pw1)
+                    auth_user.save()
                     return render(request, 'login/login.html',{'form':Login(),})
     return render(request, 'login/reset.html', {'form':form,})
 
